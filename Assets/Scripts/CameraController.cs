9122f524d2564e5fd1c3e975c47127447d5e4f57
Usuario1 <at835@scarletmail.rutgers.edu>
2021-11-02 06:04:35 -0400

    Improve player camera to orbital camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,9 +9,14 @@ public class CameraController : MonoBehaviour
 	[SerializeField] private float distanceAway;
 	[SerializeField] private float distanceUp;
 	[SerializeField] private float smooth;
-	[SerializeField] private Transform follow;
+	[SerializeField] private Transform followXform;
+	[SerializeField] private Vector3 offset = new Vector3(0f, 1.5f, 0f);
+	
+	[SerializeField] private float camSmoothDampTime = 0.1f;
+	private Vector3 velocityCamSmooth = Vector3.zero;	
 	
 	private Vector3 targetPosition;
+	private Vector3 lookDir;
 	
 	#endregion
 	
@@ -25,7 +30,7 @@ public class CameraController : MonoBehaviour
     void Start()
     {
 		
-		follow = GameObject.FindWithTag("Player").transform;
+		followXform = GameObject.FindWithTag("Player").transform;
     }
 
     // Update is called once per frame
@@ -40,19 +45,31 @@ public class CameraController : MonoBehaviour
 	
 	void LateUpdate()
 	{
-		// setting the target position
-		targetPosition = follow.position + Vector3.up * distanceUp - follow.forward * distanceAway;
+		Vector3 characterOffset = followXform.position + offset;
 		
-		Debug.DrawRay(follow.position, Vector3.up * distanceUp, Color.red);
-		Debug.DrawRay(follow.position, -1f * follow.forward * distanceAway, Color.blue);
-		Debug.DrawRay(follow.position, targetPosition, Color.magenta);
+		lookDir = characterOffset - this.transform.position;
+		lookDir.y = 0;
+		lookDir.Normalize();
+		Debug.DrawRay(this.transform.position, lookDir, Color.green);
+		
+		// setting the target position
+		targetPosition = characterOffset + followXform.up * distanceUp - lookDir * distanceAway;
+		
+		// Debug.DrawRay(followXform.position, Vector3.up * distanceUp, Color.red);
+		// Debug.DrawRay(followXform.position, -1f * followXform.forward * distanceAway, Color.blue);
+		Debug.DrawRay(followXform.position, targetPosition, Color.magenta);
 		
 		// making a smooth transition from it's current position a the position it wants to be in
-		transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
+		smoothPosition(this.transform.position, targetPosition);
 		
-		// make camera follow player
-		transform.LookAt(follow);
+		// make camera followXform player
+		transform.LookAt(followXform);
 	}
 	
+	private void smoothPosition(Vector3 fromPos, Vector3 toPos)
+	{
+		// Making a smooth transition between camera's current position and the position it wants to be in
+		this.transform.position = Vector3.SmoothDamp(fromPos, toPos, ref velocityCamSmooth, camSmoothDampTime);
+	}
 	#endregion
 }
--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -7,12 +7,18 @@ public class CharacterController : MonoBehaviour
 	#region properties (private)
 	
 	[SerializeField] private float directionDampTime = .25f;
-	
+	[SerializeField] private Camera gamecam;
+	[SerializeField] private float directionSpeed = 3.0f;
+	[SerializeField] private float rotationDegreePerSecond = 120f;
 	private Animator anim;
 	
 	private float speed = 0.0f;
+	private float direction = 0.0f;
 	private float h = 0.0f;
 	private float v = 0.0f;
+	private AnimatorStateInfo stateInfo;
+	
+    private int hashLocomotionId = 0;
 	
 	#endregion
 	
@@ -30,6 +36,8 @@ public class CharacterController : MonoBehaviour
 		if (anim.layerCount >= 2) {
 			anim.SetLayerWeight(1, 1);
 		}
+		
+		 hashLocomotionId = Animator.StringToHash("Base Layer.Locomotion");
     }
 
     // Update is called once per frame
@@ -37,20 +45,69 @@ public class CharacterController : MonoBehaviour
     {
 		if (anim)
 		{
+			stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+			
 			// Pull values from controller/keyboard
 			v = Input.GetAxis("Vertical");
 			h = Input.GetAxis("Horizontal");
 			
-			speed = new Vector2(h, v).sqrMagnitude;
+			// Translate controls stick coordinates into world/cam/character space
+            StickToWorldspace(this.transform, gamecam.transform, ref direction, ref speed);	
 			
 			anim.SetFloat("Speed", speed);
 			anim.SetFloat("Direcction", h, directionDampTime, Time.deltaTime);
 		}
     }
 	
+	void FixedUpdate()
+	{
+		// Rotate character model right or left, but only if character is moving in that direction
+		if (isInLocomotion() && ((direction >= 0 && h >= 0) || (direction < 0 && h < 0)))
+		{
+			Vector3 rotationAmount = Vector3.Lerp(Vector3.zero, new Vector3(0f, rotationDegreePerSecond * (h < 0f ? -1f : 1f), 0f), Mathf.Abs(h));
+			Quaternion deltaRotation = Quaternion.Euler(rotationAmount * Time.deltaTime);
+			this.transform.rotation = (this.transform.rotation * deltaRotation);
+		}
+	}
+	
+	
 	#endregion
 	
 	#region methods
+	
+	public void StickToWorldspace(Transform root, Transform camera, ref float directionOut, ref float speedOut)
+    {
+        Vector3 rootDirection = root.forward;
+				
+        Vector3 stickDirection = new Vector3(h, 0, v);
+		
+		speedOut = stickDirection.sqrMagnitude;		
+
+        // Get camera rotation
+        Vector3 CameraDirection = camera.forward;
+        CameraDirection.y = 0.0f; // kill Y
+        Quaternion referentialShift = Quaternion.FromToRotation(Vector3.forward, Vector3.Normalize(CameraDirection));
+
+        // Convert joystick input in Worldspace coordinates
+        Vector3 moveDirection = referentialShift * stickDirection;
+		Vector3 axisSign = Vector3.Cross(moveDirection, rootDirection);
+		
+		Debug.DrawRay(new Vector3(root.position.x, root.position.y + 2f, root.position.z), moveDirection, Color.green);
+		Debug.DrawRay(new Vector3(root.position.x, root.position.y + 2f, root.position.z), rootDirection, Color.magenta);
+		Debug.DrawRay(new Vector3(root.position.x, root.position.y + 2f, root.position.z), stickDirection, Color.blue);
+		Debug.DrawRay(new Vector3(root.position.x, root.position.y + 2.5f, root.position.z), axisSign, Color.red);
+		
+		float angleRootToMove = Vector3.Angle(rootDirection, moveDirection) * (axisSign.y >= 0 ? -1f : 1f);
+		
+		angleRootToMove /= 180f;
+		
+		directionOut = angleRootToMove * directionSpeed;
+	}	
+	
+	public bool isInLocomotion()
+    {
+        return stateInfo.nameHash == hashLocomotionId;
+    }
 	
 	#endregion
 }
