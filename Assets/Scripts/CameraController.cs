5d052775704d079b3c188f834652e903f55e5b71
Usuario1 <at835@scarletmail.rutgers.edu>
2021-11-02 04:18:18 -0400

    Add basic camera follow up

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+	#region properties (private)
 	
-	[SerializeField] private float mouseSensitivity = 0.0f;
 	[SerializeField] private float distanceAway;
 	[SerializeField] private float distanceUp;
 	[SerializeField] private float smooth;
 	[SerializeField] private Transform follow;
 	
 	private Vector3 targetPosition;
-	private Transform parent;
 	
+	#endregion
+	
+	#region properties (private)
+	
+	#endregion
+	
+	#region unity event functions
 	
     // Start is called before the first frame update
     void Start()
     {
-		parent = transform.parent;
-		Cursor.lockState = CursorLockMode.Locked;
 		
 		follow = GameObject.FindWithTag("Player").transform;
     }
@@ -27,26 +31,28 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Rotate();
+        
     }
 	
-	void Rotate()
-	{
-		float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-		
-		parent.Rotate(Vector3.up, mouseX);
-	}
+	#endregion
+	
+	#region methods
 	
 	void LateUpdate()
 	{
+		// setting the target position
 		targetPosition = follow.position + Vector3.up * distanceUp - follow.forward * distanceAway;
 		
 		Debug.DrawRay(follow.position, Vector3.up * distanceUp, Color.red);
 		Debug.DrawRay(follow.position, -1f * follow.forward * distanceAway, Color.blue);
 		Debug.DrawRay(follow.position, targetPosition, Color.magenta);
 		
+		// making a smooth transition from it's current position a the position it wants to be in
 		transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
 		
+		// make camera follow player
 		transform.LookAt(follow);
 	}
+	
+	#endregion
 }
